4c5d443cf157e7748dbf464f51e67a2592f47abb
Hari_karthyk <54505967+Harikarthyk@users.noreply.github.com>
2020-06-10 12:11:23 +0530

    Add files via upload

---
+++ COVID-19Tracker/COVID19Tracker.js
@@ -0,0 +1,109 @@
+function indiaCovidDetails(){
+	$.getJSON("https://api.covid19india.org/data.json",function(data){
+	
+		var allState = data.statewise;
+	
+		for( var i=1;i<38;i++ ) {
+
+			var state = allState[i].state ;
+			var active = allState[i].active ;
+			var total = allState[i].confirmed ;
+			var recovered = allState[i].recovered ;
+			var deaths = allState[i].deaths ;
+			var tableRow = "<tr><td>"+ i +
+			"</td><td class=stateName total="+total+" active="+ active +" deaths="+deaths +">" + state + "</td><td>" + total + "</td><td>"+ active +"</td><td>"+ recovered +"</td><td>"+ deaths +"</td></tr>" ; 
+			$('.table').append(tableRow) ;
+
+		}
+		
+		$('td.stateName').click(function(){
+			localStorage.setItem('state', $(this).text() );
+			localStorage.setItem( 'stateTotal' , $(this).attr('total') ) ;
+			localStorage.setItem( 'stateActive' , $(this).attr('active') ) ;
+			localStorage.setItem( 'stateDeaths' , $(this).attr('deaths') ) ;
+			window.location = "IndividualDistrict-India/individualDistrict.html" ;	
+		});
+		
+	});
+
+}
+function globalCovidDetails(){
+	
+	$.getJSON("https://coronavirus-tracker-api.herokuapp.com/v2/locations",function(data){
+	
+		var total = "Total "+data.latest.confirmed ;
+		var deaths = "Death "+data.latest.deaths;
+		var globalContent = "<p class=global-box>" + total + "</p><p class=global-box>"+ deaths +"</p>" ;
+		$('.global-content').append( globalContent ) ;
+		var popupTotal = Math.round( 7800000000/data.latest.confirmed );
+		popupTotal = "For Every "+popupTotal+" - 1 people gets Affected " ;
+		$('.popup-total').append(popupTotal) ;
+		// alert( popupTotal ) ;
+		var popupDeaths = Math.round( 7800000000/data.latest.deaths ) ;
+		popupDeaths = "For Every "+popupDeaths+" - 1 people dies " ;
+		$('.popup-deaths').append(popupDeaths) ;
+	
+	
+	});	
+
+}
+function globalCountryDetails( country ){
+	$.getJSON( "https://coronavirus-tracker-api.herokuapp.com/v2/locations?country="+country,function(data){
+		$('.country-details-head').text(country) ;
+		var total = data.latest.confirmed ;
+		var death = data.latest.deaths;
+		console.log(data);
+		$('.country-details-main1').text( "Total "+total+" Death "+death ) ;
+		var affectRatio = Math.round( data.locations[0].country_population/data.latest.confirmed ) ;
+		$('.country-details-main2').text( "For Every "+affectRatio+" people 1 gets Affected " ) ;
+		affectRatio = Math.round( data.locations[0].country_population/data.latest.deaths ) ;
+		$('.country-details-main4').text( "For Every "+affectRatio+" people 1 dies " ) ;
+		var lastUpdated = data.locations[0].last_updated ;
+		lastUpdated = lastUpdated.substring(0,10) ;
+		$('.country-details-main3').text( "Last Updated : "+lastUpdated ) ;
+		
+		// alert(total+" "+death) ;
+	});
+}
+$(document).keydown(function(e){
+    if(e.which === 123){
+       return false;
+    }
+});
+$(document).bind("contextmenu",function(e) {
+ e.preventDefault();
+});
+$(document).ready(function(){
+	$('#india').css( 'display' , 'none' ) ;	
+	globalCovidDetails();	
+	$('.head-india').on('click',function(){
+		$('.india').show() ;
+		$('#india').css( 'display' , 'flex !important' ) ;
+		$('.global').hide();
+		indiaCovidDetails() ;
+	});
+	
+	$('.head-global').on('click',function(){
+		$('.india').hide() ;
+		$('.global').show();
+		$('#india').css( 'display' , 'none !important' ) ;	
+	});
+	$('.global-box').mouseover(function(){
+		$('.popup-total').show();
+		$('.popup-deaths').show();	
+	}).mouseout(function(){
+		$('.popup-total').hide();
+		$('.popup-deaths').hide();
+	});
+	$('.input').on('keyup',function(event){
+		if( event.keyCode===13 ){
+			var country = $('.input').val();
+			globalCountryDetails(country) ;	
+		}
+	});
+	$('i').on( 'click',function(){
+		var country = $('.input').val();
+		// alert(country) ;
+		globalCountryDetails(country) ;
+	}) ;
+});
---
+++ COVID-19Tracker/IndividualDistrict-India/COVID19India.js
@@ -0,0 +1,64 @@
+$(document).keydown(function(e){
+    if(e.which === 123){
+       return false;
+    }
+});
+$(document).bind("contextmenu",function(e) {
+ e.preventDefault();
+});
+$(document).ready(function() {
+	
+	$.getJSON("https://api.covid19india.org/state_district_wise.json",function(data){
+
+		var state = localStorage.getItem('state');
+		var stateDetails = data[state].districtData ;
+		var stateTotal = localStorage.getItem('stateTotal') ;	
+		var stateActive = localStorage.getItem('stateActive') ;
+		var stateDeaths = localStorage.getItem('stateDeaths') ;
+		$('.state').text( state ) ;
+		$('.popup').append( "Total :  "+stateTotal+" Active :  "+stateActive+" Deaths :  "+stateDeaths  ) ;
+		for (var districtName in stateDetails) {
+ 			
+			if( districtName=="Other State" ) continue ;
+
+ 			var total = stateDetails[districtName].confirmed ;
+ 			total = "Total : "+total ;
+ 			
+ 			var active = stateDetails[districtName].active ;
+ 			var bgColorChange = active ;
+ 			active = "Active : " +active ;
+ 			
+ 			var recovered = stateDetails[districtName].recovered ;
+			recovered = "Recovered : " + recovered ;
+			
+			//this line is used to remove all spaces so that while includeing in id it includes full name 
+			//by using replace() method only on space are removed for multiple below statement is used
+			
+			tempName = districtName.split(" ").join("");
+			var districtBody = "<div class=district-head >"+"<span id="+tempName+ ">"+ districtName+"</span>" +"<div class=district-total>"+ total +
+			"</div><div class=district-active>"+ active +
+			"</div><div class=district-cured>"+ recovered +"</div></div>" ;
+			
+			$('.district-body ').append( districtBody ) ;
+			var id = "#"+tempName ;
+			// console.log( id+" "+check ) ;
+			if( bgColorChange>=50 )
+				$(id).parent().css( 'background-color','#ff4f4f' );
+			else if( bgColorChange>=15 )
+				$(id).parent().css( 'background-color','#ffbd66' );		
+			else if( bgColorChange<15 ) 
+				$(id).parent().css( 'background-color','#71c650' );		
+			
+		}
+
+
+	});
+
+	$('.state').mouseover(function(){
+		$('.popup').show();
+		// alert("in") ;
+	}).mouseout(function() { 
+    $(".popup").hide();
+	});
+
+});
